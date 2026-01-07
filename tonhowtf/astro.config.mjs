b47bd2ec95b770782a36ef10c24fe45b4239dda8
tonhowtf <tonhowtf@gmail.com>
2026-01-07 12:58:32 -0300

    fix: update base URL handling and improve asset paths
    - Adjusted the base URL in astro.config.mjs to include a trailing slash for consistency.
    - Updated links in [slug].astro and index.astro to use import.meta.env.BASE_URL for dynamic path resolution.
    - Added generated index.html and favicon.svg files to the dist directory.
    - Included a test image in the dist/images directory for demonstration purposes.

--- tonhowtf/astro.config.mjs
+++ tonhowtf/astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://tonhowtf.github.io",
-  base: "/tonhowtf",
+  site: 'https://tonhowtf.github.io',
+  base: '/tonhowtf/',
 });
